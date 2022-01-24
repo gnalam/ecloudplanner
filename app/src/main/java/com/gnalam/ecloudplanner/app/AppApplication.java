69ead18c472fbffa0b250187e15d903fba756351
Aldrine Einsteen <aldrine@gnalam.com>
2022-01-24 00:30:48 +0000

    trying sec audits with v16.0

--- app/src/main/java/com/gnalam/ecloudplanner/app/AppApplication.java
+++ app/src/main/java/com/gnalam/ecloudplanner/app/AppApplication.java
@@ -1,12 +1,16 @@
 package com.gnalam.ecloudplanner.app;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AppApplication {
+    private static final Logger log = LogManager.getLogger(AppApplication.class);
 
     public static void main(String[] args) {
+        log.info("Starting eCloudPlanner backend");
         SpringApplication.run(AppApplication.class, args);
     }
 
